b132a7f8dba06ed3f61415238ab28d0675fec171
Natalia <nataliya.a.sokolova@gmail.com>
2018-09-20 09:25:16 -0700

    add input for searching module

---
+++ src/app/grocery/grocery.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-grocery',
+  templateUrl: './grocery.component.html',
+  styleUrls: ['./grocery.component.css']
+})
+export class GroceryComponent implements OnInit {
+  
+  task: string;
+  tasks = [];
+
+  onClick(){
+    this.tasks.push({name: this.task});
+    this.task = '';
+  }
+  constructor() { }
+
+  ngOnInit() {
+  }
+}
+
+
+
+
